ae1f7c6bcdfc8f9fefbb95574bd6c35844b63dbc
Hugues Delorme <delorme.hugues@fougsys.fr>
2012-11-26 13:11:29 +0100

    Fix Wunknown-pragmas gcc warnings

--- src/tritri.c
+++ src/tritri.c
@@ -152,11 +152,9 @@ int coplanar_tri_tri(float N[3],float V0[3],float V1[3],float V2[3],
    short i0,i1;
    /* first project onto an axis-aligned plane, that maximizes the area */
    /* of the triangles, compute indices: i0,i1. */
-#pragma warning( disable : 4244 )
    A[0]=fabs(N[0]);
    A[1]=fabs(N[1]);
    A[2]=fabs(N[2]);
-#pragma warning( default : 4244 )
    if(A[0]>A[1])
    {
       if(A[0]>A[2])  
@@ -263,12 +261,11 @@ int tri_tri_intersect(float V0[3],float V1[3],float V2[3],
   CROSS(D,N1,N2);
 
   /* compute and index to the largest component of D */
-#pragma warning( disable : 4244 )
   max=fabs(D[0]);
   index=0;
   b=fabs(D[1]);
   c=fabs(D[2]);
-#pragma warning( default : 4244 )
+
   if(b>max) max=b,index=1;
   if(c>max) max=c,index=2;
 
